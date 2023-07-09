75a5a8da9b7cca14cc2d591375e0cefb665b3b18
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-09 15:00:41 -0700

    hson server db added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className='text-red-500'>Hello</div>;
+  return (
+    <div className='text-red-500'>
+      My Media
+      <a href='#' className='logo'>
+        <img
+          className='logo'
+          src='/vite.svg'
+          alt='vite logo'
+        />
+      </a>
+    </div>
+  );
 }
 
 export default App;
